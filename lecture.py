fba97a7389ecc5af2a547de006ef1a6d78b026df
Bryan.BlinDorard <bryanblindorard@gmail.com>
2022-04-14 18:26:48 +0200

    amélioration du code(l'encodage des caractères et de la recherche)

--- lecture.py
+++ lecture.py
@@ -1,7 +1,9 @@
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
 
 filename = 'data2.json'
-json_data = open(filename).read()
+json_data = open(filename, encoding="utf8").read()
 data = json.loads(json_data)
 
 dico_de_gens = dict() 
@@ -23,7 +25,7 @@ def tout_les_gens_du_film(titre):
     if data[film]["title"] == titre:
       for gens in data[film]["cast"]:
         gens = clearNom(gens) 
-        gens = gens.encode('latin1').decode('utf8') 
+        
         list.append(gens) 
   return list
 
@@ -31,8 +33,7 @@ for film in range(len(data)):
   titre = data[film]["title"]
   for gens in data[film]["cast"]:
     gens = clearNom(gens) # On supprime les [] et le |
-    gens = gens.encode('latin1').decode('utf8') # Ce code permet de convertir les caractères spéciaux en utf8
-
+    
 
     liste_de_gens_du_film = tout_les_gens_du_film(titre)
     if gens not in dico_de_gens:
@@ -51,3 +52,13 @@ dico_final = clearDoublon(dico_de_gens) # dico final avec les doublons en moins
 
 print(dico_final)
 
+# G = nx.Graph()
+
+# for elem in dico_final:
+#   G.add_node(elem)
+  
+# for elem in dico_final:
+#   for elem2 in dico_final[elem]:
+#     G.add_edge(elem,elem2)
+
+# nx.draw(G,with_labels=True)      
